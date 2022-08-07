ee88245301ba960968bb3026b17701870a96c7db
Dima <dimonussir5@gmail.com>
2022-08-07 16:07:24 +0300

    lambok, json

---
+++ src/AnnotationAndLombok/second.java
@@ -0,0 +1,8 @@
+package AnnotationAndLombok;
+
+public class second {
+    @Deprecated
+    public int com(){
+        return 0;
+    }
+}
--- src/SystemCompelationMaven/Main.java
+++ src/SystemCompelationMaven/Main.java
@@ -1,7 +1,52 @@
 package SystemCompelationMaven;
 
-public class Main {
-    public static void main(String[] args) {
+import Lambda.Employee;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
-    }
+import javax.xml.crypto.Data;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+
+public class Main {
+   //        private static String staffFile = "staff.txt";
+  //  public static void main(String[] args) {
+       // try {
+
+/**
+ *   try {
+ *             String data = getDataFromFile("staff.json");
+ *             JSONParser parser = new JSONParser();
+ *             JSONArray array = (JSONArray) parser.parse(data);
+ *             for (Object item : array){
+ *                 JSONObject jsonObject = (JSONObject) item;
+ *                 System.out.println(jsonObject.get("name"));
+ *             }
+ *                 }
+ *         catch (Exception ex){
+ *             ex.printStackTrace();
+ *         } }
+ */
+/**
+ private static String getDataFromFile (String path) { //превращает файл в строчку
+ StringBuilder builder = new StringBuilder(); // будем добавлять строки нашего файла
+
+ try {
+ List<String> lines = Files.readAllLines(Paths.get()); // readAllLines возвращал коллекцию со строками
+ for (String line : lines) {
+ builder.append(line);// добавление строк из файла
+ }
+ } catch (Exception ex) {
+ ex.printStackTrace();
+ }
+ return builder.toString();
+ }
+
+
+ */
 }
