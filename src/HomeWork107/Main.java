94f011096c0f4e306d5cf32a4a1c6403e30c9a33
Dima <dimonussir5@gmail.com>
2022-08-17 01:34:11 +0300

    tread Stream new

--- src/HomeWork107/Main.java
+++ src/HomeWork107/Main.java
@@ -1,3 +1,5 @@
+package HomeWork107;
+
 public class Main {
     public static void main(String[] args) {
 
--- src/HomeWork107/Metods.java
+++ src/HomeWork107/Metods.java
@@ -1,3 +1,5 @@
+package HomeWork107;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
